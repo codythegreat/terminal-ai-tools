a14a348669cec0499846f07321959bac7ccdf4e2
codythegreat <maxiecody@gmail.com>
2023-03-03 19:11:29 -0600

    No longer passes output of gpt.py into speak.sh--handle it all in python

--- gpt-speak.sh
+++ gpt-speak.sh
@@ -4,4 +4,4 @@ arecord -d 10 -f cd output.wav
 
 whisper --model tiny.en --output_format txt output.wav
 
-cat output.wav.txt | tr '\n' ' ' | sed "s/'//g" | xargs -I {} ./gpt.py {} | tee >(xargs -I {} ./speak.sh "{}")
+cat output.wav.txt | tr '\n' ' ' | sed "s/'//g" | xargs -I {} ./gpt.py {}
